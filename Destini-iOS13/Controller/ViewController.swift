906710376019b6199174904e0249b990b868812c
AnnaMelekh <annasmelekhina@gmail.com>
2024-09-27 17:43:14 +0200

    implementing MVC, creating methods

--- Destini-iOS13/Controller/ViewController.swift
+++ Destini-iOS13/Controller/ViewController.swift
@@ -2,23 +2,38 @@
 //  ViewController.swift
 //  Destini-iOS13
 //
-//  Created by Angela Yu on 08/08/2019.
-//  Copyright © 2019 The App Brewery. All rights reserved.
-//
+// by Anna M//
 
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     @IBOutlet weak var storyLabel: UILabel!
     @IBOutlet weak var choice1Button: UIButton!
     @IBOutlet weak var choice2Button: UIButton!
     
+    var storyBrain = StoryBrain()
+
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        updateUI()
+           
     }
-
-
+    
+    
+    @IBAction func choiceMade(_ sender: UIButton) {
+        let userChoice = sender.currentTitle!
+        storyBrain.nextStory(userChoice: userChoice)
+        
+        updateUI()
+    }
+    func updateUI() {
+        storyLabel.text = storyBrain.getStoryText()
+        choice1Button.setTitle(storyBrain.getChoiceText(choiceNumber: 1), for: .normal)
+        choice2Button.setTitle(storyBrain.getChoiceText(choiceNumber: 2), for: .normal)
+        
+    }
 }
 
